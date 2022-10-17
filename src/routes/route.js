c8878d0e069c9797c456bd2092c73232390c9622
ardravrahul <silentrahul78@gmail.com>
2022-10-17 23:01:46 +0530

    assignment api

--- src/routes/route.js
+++ src/routes/route.js
@@ -37,7 +37,59 @@ router.get('/student-details/:name', function(req, res){
     console.log("This is the request ", requestParams)
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
-})
 
-module.exports = router;
+// 1. this api fetch all array of movies
+
+router.get('/movies',function(req,res){
+    res.send('["dabbang","fukrey","rocky","upkar","om"]')
+});
+
+//2. this api fetch all array of movies by index no.
+
+router.get('/movies/:movieId', function(req,res){
+    mov=["dabbang","fukrey","rocky","upkar","om"]
+    let value= req.params.movieId;
+    if(value>mov.length-1){
+        res.send('"does not exist"')
+    }
+    else{
+        res.send(mov[value])
+    }
+});
+
+//3. this api will getch all movies from array by indeaxid
+router.get('/moviez',function(req,res){
+    res.send([{id: 1, name: 'the shine'},{id :2,name:'incredible'},{id:3,name:'3 ideots'},{id:4,name:'radhe'},{id:5, name:'the gandhi'}])
+
+});
+
+//4. this API will fetch all movies from array of objects by indexId
+router.get('/films/:filmId',function(req,res){
+    let movi= [{id: 1, name: 'the shine'},{id :2,name:'incredible'},{id:3,name:'3 ideots'},{id:4,name:'radhe'},{id:5, name:'the gandhi'}]
+    let value= req.params.filmId;
+    let found=false;
+    for(i=0;i<movi.length;i++){
+        if(movi[i].id==value){
+            found=true
+            res.send(movi[i])
+            break
+        } 
+    }
+     if(found==false){
+
+     res.send('no movie exist with this id')}
+});
+
+
+
+
+
+
+
+
+
+
+    
+
+
+module.exports = router
