0e4fa35400fa610c5e4fc7517642ced4040dcd4b
MdFaizan20 <beingfaizan20@gmail.com>
2022-11-14 12:40:16 +0530

    index

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const route = require('./routes/route.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://Divyajeetkumar:<12345>@project1.rxkr5ev.mongodb.net/test", { useNewUrlParser: true })
+    .then(() => console.log('mongodb running perfectly'))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
