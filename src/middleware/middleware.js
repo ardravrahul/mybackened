a6218da23ad22b143ea8d6bf2a6103da62c0f61e
ardravrahul <silentrahul78@gmail.com>
2022-11-20 19:52:49 +0530

    middleware correction

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,41 +1,47 @@
-// const jwt = require("jsonwebtoken")
-// const authorization = async function (req, res, next) {
-//     try {
-//         const token = req.headers["x-api-key"]
-//         if (!token) {
-//             return res.status(401).send({ status: false, msg: "pls provide token" })
-//         }
-//         const decoded =   jwt.verify(token, "")
-//         if (!decoded) {
-//             return res.status(403).send({ status: false, msg: "authorization failed!, Pls provide Valid token" })
-//         }
-//         next()
-//     }
-//     catch (err) {
-//         return res.status(500).send({ status: false, msg: err.message })
-//     }
-
-
-// }
-// module.exports = { authorization }
-
-const jwt = require("jsonwebtoken")
-const authorization = async function (req, res, next) {
+const jwt =require("jsonwebtoken")
+const blogModel =require("../models/blogModel")
+const authentication = function (req, res, next) {
     try {
-        const token = req.headers["x-api-key"]
-        if (!token) {
-            return res.status(401).send({ status: false, msg: "pls provide token" })
-        }
-        const decoded =   jwt.verify(token, "mavrik")
-        if (!decoded) {
-            return res.status(403).send({ status: false, msg: "authorization failed!, Pls provide Valid token" })
-        }
-        next()
-    }
-    catch (err) {
-        return res.status(500).send({ status: false, msg: err.message })
-    }
+        let token = req.headers["x-api-key"];
+        if (!token) return res.status(401).send({ status: false, msg: " token must be present for authentication " })
 
-
+        jwt.verify(token, "mavrik", function (err, decodedToken) {
+            if (err) {
+                return res.status(400).send({ status: false, msg: "token invalid" });
+            } 
+        
+                req.decodedToken = decodedToken
+                next() 
+        })
+    } catch (err) {
+        res.status(500).send({ status: false, msg: err.message })
+    }
 }
-module.exports = { authorization }
+
+//=========================authorisation ======================================================
+
+
+const authorization = async function (req, res, next) {
+    try{
+        
+        let blogId = req.params.blogId
+        let authorLoggedIn = req.decodedToken.authorId
+    
+        let findBlog = await blogModel.findById(blogId)
+
+        if(!findBlog)
+        {return res.status(404).send("status:false, msg:Author's blog not found")}
+        let authorId = findBlog.authorId
+        
+    
+        if (!authorId === authorLoggedIn)
+            return res.status(403).send({ stauts: false, msg: "User and user's-token in not matched" })
+        next()
+
+     }
+    catch(error) {
+        return res.status(500).send({ status: false, msg: error.message});
+    }
+    };
+
+    module.exports = {authentication,authorization}
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,20 +4,20 @@ const router =express.Router()
 
 const authorController = require("../controllers/authorController")
 const blogController = require("../controllers/blogController")
-const middleware =require("../middleware/middleware")
+const m =require("../middleware/middleware")
 
 router.post("/authors",authorController.createAuthor) // for author creation
 
 router.post("/login",authorController.authorLogin) //for login author
 
-router.post("/blogs",middleware.authentication,blogController.createBlog) //for blogs creation 
+router.post("/blogs",m.authentication,blogController.createBlog) //for blogs creation 
 
-router.get("/filterBlogs",middleware.authentication,blogController.filterBlogs) //for fetching blogs 
+router.get("/filterBlogs",m.authentication,blogController.filterBlogs) //for fetching blogs 
 
-router.put("/blogs/:blogId",middleware.authentication,blogController.updateBlog) //for updating blogs
+router.put("/blogs/:blogId",m.authentication,blogController.updateBlog) //for updating blogs
 
-router.delete("/blogs/:blogId",middleware.authentication,blogController.blogDeletionById) //for deletion of  blog with Id
+router.delete("/blogs/:blogId",m.authentication,blogController.blogDeletionById) //for deletion of  blog with Id
 
-router.delete("/blogs",middleware.authentication,blogController.deleteBlog) //for deletion of  blog with query
+router.delete("/blogs",m.authentication,blogController.deleteBlog) //for deletion of  blog with query
 
 module.exports=router   
